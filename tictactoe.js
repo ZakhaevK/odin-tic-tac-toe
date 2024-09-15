4bcf7a6e744e1c239659c1ca552d86c9b31a98ef
ZakhaevK <zkupke@gmail.com>
2024-09-15 14:28:53 +1000

    Add player and gameboard (IIFE) factory functions - Currently player will have a symbol attached, may change though - Players will store their number of wins - Gameboard uses the symbol to change an value at a designated array position

---
+++ tictactoe.js
@@ -0,0 +1,46 @@
+
+const gameBoard = (function () {
+  const board = [['', '', '']
+  ['', '', '']
+  ['', '', '']]
+
+  const getBoard = (() => {
+    for (row in board) {
+      console.log(row);
+    }
+  })
+  const takePosition = (function (player, x, y) {
+    if (x > 2 || x < 0 || y > 2 || y < 0) {
+      return console.log("Invalid coordinates")
+    } else {
+      board[x][y] = player.getSymbol();
+    }
+  })
+  return {getBoard, takePosition}
+})
+
+
+function createPlayer (name, symbol) {
+  const name = name;
+  const symbol = symbol;
+  const wins = 0;
+
+  const getName = (() => {
+    return name;
+  })
+
+  const getSymbol = (() => {
+    return symbol;
+  })
+
+  const getWins = (() => {
+    return wins;
+  })
+
+  const wonGame = (() => {
+    wins++;
+  })
+
+  return {getName, getSymbol, getWins, wonGame};
+}
+
