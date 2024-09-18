32e49caaedf76034cbd686176e2b6414c7b89087
ZakhaevK <zkupke@gmail.com>
2024-09-18 13:50:53 +1000

    Add mostly complete console/prompt game logic - Error checking is weak, but mainly due to changes from HTML implementation - Currently lacks check of symbol in a position to prevent other players stealing spot

--- tictactoe.js
+++ tictactoe.js
@@ -1,56 +1,125 @@
-
 const gameBoard = (function () {
-  const board = [ ['', '', '']
-                  ['', '', '']
-                  ['', '', ''] ]
+  let board = [ 
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
 
-  const getBoard = (() => {
-    for (row in board) {
+  const printBoard = () => {
+    for (let row of board) {
       console.log(row);
     }
-  })
+  };
 
-  const takePosition = (function (player, x, y) {
+  const takePosition = (player, x, y) => {
     if (x > 2 || x < 0 || y > 2 || y < 0) {
-      return console.log("Invalid coordinates")
+      console.log("Invalid coordinates");
     } else {
       board[x][y] = player.getSymbol();
     }
-  })
+  };
 
-  const clearBoard = (() => {
-    board = [ ['', '', '']
-              ['', '', '']
-              ['', '', ''] ]
-  })
-  
-  return {getBoard, takePosition, clearBoard}
-})
+  const clearBoard = () => {
+    board = [ 
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ];
+  };
 
-function createPlayer (name, symbol) {
-  const name = name;
-  const symbol = symbol;
-  const wins = 0;
+  const checkWinner = (player) => {
+    let count = 0;
 
-  const getName = (() => {
-    return name;
-  })
+    // Check rows for winner
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] == player.getSymbol()) { count++; }
+        if (count == 3) { return true; }
+      }
+      count = 0;
+    }
 
-  const getSymbol = (() => {
-    return symbol;
-  })
+    // Check columns for winner
+    count = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[j][i] == player.getSymbol()) { count++; }
+        if (count == 3) { return true; }
+      }
+      count = 0;
+    }
 
-  const getWins = (() => {
-    return wins;
-  })
+    // Check diagonal lines
+    count = 0;
+    for (let i = 0; i < 3; i++) {
+      if (board[i][i] == player.getSymbol()) { count++; }
+      if (count == 3) { return true; }
+    }
 
-  const wonGame = (() => {
+    count = 0;
+    for (let i = 0, j = 2; i < 3; i++, j--) {
+      if (board[i][j] == player.getSymbol()) { count++; }
+      if (count == 3) { return true; }
+    }
+
+    return false;
+  };
+
+  return { printBoard, takePosition, clearBoard, checkWinner };
+})();
+
+function createPlayer(name, symbol) {
+  let wins = 0;
+
+  const getName = () => name;
+
+  const getSymbol = () => symbol;
+
+  const getWins = () => wins;
+
+  const wonGame = () => {
     wins++;
-  })
+  };
 
-  return {getName, getSymbol, getWins, wonGame};
+  return { getName, getSymbol, getWins, wonGame };
 }
 
-function game (gameBoard, Player1, Player2) {
-  
+function createGame(gameBoard, player1, player2) {
+  let currentPlayer = player1;
+
+  const playTurn = () => {
+    let posX = parseInt(prompt(`${currentPlayer.getName()}, enter row:`));
+    let posY = parseInt(prompt(`${currentPlayer.getName()}, enter column:`));
+    if (posX < 0 || posX > 2 || posY < 0 || posXY> 2 ) {
+      console.log("Invalid coordinates, try again")
+      posX = parseInt(prompt(`${currentPlayer.getName()}, enter row:`));
+      posY = parseInt(prompt(`${currentPlayer.getName()}, enter column:`));
+    }
+    gameBoard.takePosition(currentPlayer, posX, posY);
+  };
+
+  const playGame = () => {
+    let gameFinished = false;
+    let turns = 0;
+    while (!gameFinished && turns < 9) {
+      playTurn();
+      gameBoard.printBoard();
+      gameFinished = gameBoard.checkWinner(currentPlayer);
+      if (gameFinished) {
+        currentPlayer.wonGame();
+        console.log(`The winner is ${currentPlayer.getName()}`);
+        return;
+      }
+      turns++;
+      currentPlayer = (currentPlayer === player1) ? player2 : player1;
+    }
+    console.log("Game is tied!");
+  };
+
+  return { playTurn, playGame };
 }
+
+const player1 = createPlayer('Player 1', 'X');
+const player2 = createPlayer('Player 2', 'O');
+const game = createGame(gameBoard, player1, player2);
+game.playGame();
